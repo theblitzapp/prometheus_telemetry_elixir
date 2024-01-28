0bdd8407a4dee5452a6a66b8fe2613ca873621dc
Mika Kalathil <mika@kalathil.me>
2024-01-28 00:04:26 -0800

    update library detection to make it less strict
    trying to fix swoos herrors

--- lib/prometheus_telemetry/utils.ex
+++ lib/prometheus_telemetry/utils.ex
@@ -2,11 +2,6 @@ defmodule PrometheusTelemetry.Utils do
   @moduledoc false
 
   def app_loaded?(app_name) do
-    ensure_application_loaded?(app_name) and
-    Enum.any?(Application.loaded_applications(), fn {dep_name, _, _} -> dep_name === app_name end)
-  end
-
-  defp ensure_application_loaded?(app_name) do
     case Application.ensure_loaded(app_name) do
       :ok -> true
       _ -> false
